4e2245e8ab1f6bc39ba06ebbc3ddddb741d48551
regulus735 <regulus735@gmail.com>
2023-05-31 14:15:16 +0200

    Update HomeController.cs

--- Aplikacje_WWW_S-kolokwium2/Kolokwium/Kolokwium.Web/Controllers/HomeController.cs
+++ Aplikacje_WWW_S-kolokwium2/Kolokwium/Kolokwium.Web/Controllers/HomeController.cs
@@ -28,6 +28,37 @@ namespace Kolokwium.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+        [HttpGet]
+[Authorize (Roles = "Admin")]
+public IActionResult AddOrEditSubject (int? id = null) {
+var teachersVm = _teacherService.GetTeachers ();
+ViewBag.TeachersSelectList = new SelectList (teachersVm.Select (t => new {
+Text = $"{t.FirstName} {t.LastName}",
+Value = t.Id
+}), "Value", "Text");
+if (id.HasValue) {
+var subjectVm = _subjectService.GetSubject (x => x.Id == id);
+ViewBag.ActionType = "Edit";
+return View (Mapper.Map<AddOrUpdateSubjectVm> (subjectVm));
+}
+ViewBag.ActionType = "Add";
+return View ();
+}
+public IActionResult Details (int id) {
+var subjectVm = _subjectService.GetSubject (x => x.Id == id);
+return View (subjectVm);
+}
+[HttpPost]
+[ValidateAntiForgeryToken]
+[Authorize (Roles = "Admin")]
+public IActionResult AddOrEditSubject (AddOrUpdateSubjectVm addOrUpdateSubjectVm) {
+if (ModelState.IsValid) {
+_subjectService.AddOrUpdateSubject (addOrUpdateSubjectVm);
+return RedirectToAction ("Index");
+}
+return View ();
+}
 
         
     }
